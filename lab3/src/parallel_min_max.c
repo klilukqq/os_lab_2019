35f3a0b45a04ca5db3c8c9232b09c4a608d84d90
klilukqq <klilukqq@gmail.com>
2020-05-10 14:51:32 +0000

    update1

--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -42,16 +42,33 @@ int main(int argc, char **argv) {
             seed = atoi(optarg);
             // your code here
             // error handling
+            if (seed <= 0) {
+                printf("seed is a positive number\n");
+                return 1;
+            }
             break;
           case 1:
             array_size = atoi(optarg);
             // your code here
             // error handling
+            if (array_size <= 0) {
+                printf("array_size is a positive number\n");
+                return 1;
+            }
             break;
           case 2:
             pnum = atoi(optarg);
             // your code here
             // error handling
+            if (pnum <= 0) {
+                printf("pnum is a positive number\n");
+                return 1;
+            }
+            if(pnum > array_size){
+                printf("pnum must be less than array_size\n");
+                pnum = array_size;
+                printf("pnum is equal to array_size now (%d)\n", array_size);
+            }
             break;
           case 3:
             with_files = true;
@@ -91,6 +108,17 @@ int main(int argc, char **argv) {
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
 
+  //adding pipe if need
+  int pipefd[2];
+  if(!with_files)
+  {
+      pipe(pipefd);
+  }
+  else
+  {
+      fopen("numbers.txt","w");
+  }
+
   for (int i = 0; i < pnum; i++) {
     pid_t child_pid = fork();
     if (child_pid >= 0) {
@@ -98,13 +126,29 @@ int main(int argc, char **argv) {
       active_child_processes += 1;
       if (child_pid == 0) {
         // child process
-
         // parallel somehow
+        struct MinMax minmax = GetMinMax(array, array_size/pnum*i, array_size/pnum*(i+1));
 
         if (with_files) {
           // use files here
+            FILE *fp;
+            if((fp = fopen("numbers.txt","a+")) != NULL)
+            {
+                fprintf(fp, "%d\n%d\n", minmax.min, minmax.max);
+            }
+            else {
+                printf("Can't open file\n");
+                return 1;
+            }
+            fclose(fp);
+
         } else {
+
           // use pipe here
+            close(pipefd[0]);
+            write(pipefd[1], &minmax.min, 16);
+            write(pipefd[1], &minmax.max, 16);
+            close(pipefd[1]);
         }
         return 0;
       }
@@ -117,6 +161,8 @@ int main(int argc, char **argv) {
 
   while (active_child_processes > 0) {
     // your code here
+    int st = 0;
+    wait(&st);
 
     active_child_processes -= 1;
   }
@@ -131,8 +177,35 @@ int main(int argc, char **argv) {
 
     if (with_files) {
       // read from files
+        FILE *fp;
+        int a = -1;
+        if((fp = fopen("numbers.txt","r")) != NULL)
+        {
+            for(int k = 0; k < pnum; k++)
+            {
+            fscanf(fp, "%d\n", &a);
+            if (a < min_max.min) min_max.min = a;
+            fscanf(fp, "%d\n", &a);
+            if (a > max) max = a;
+            }
+            fclose(fp);
+        }
+        else
+        {
+            printf("Can't open file\n");
+            return 1;
+        }
+
     } else {
       // read from pipes
+        int a;
+        close(pipefd[1]);
+        while (read(pipefd[0], &a, 16) > 0)
+        {
+            if (a < min) min = a;
+            if (a > max) max = a;
+        }
+        close(pipefd[0]);
     }
 
     if (min < min_max.min) min_max.min = min;
